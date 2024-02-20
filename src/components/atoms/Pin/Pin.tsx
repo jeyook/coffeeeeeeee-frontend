6ea3e5327c66ee4c3b7ce81fd86a4e3ad894ab41
JISUlicious <ljshong@hanmail.net>
2024-02-20 11:39:13 +0900

    feat: atom/Pin 작성, 기본 스타일 적용

---
+++ src/components/atoms/Pin/Pin.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+
+type PinProps = {
+  content: string;
+  onClick: () => void; // 상위 컴포넌트에서 함수 정의하여 넣어줄 것
+  backgroundColor?: string;
+  textColor?: string;
+};
+
+const Pin = ({ content, backgroundColor, textColor, onClick }: PinProps) => {
+  // content가 10글자를 넘는 경우 축약
+  return (
+    <PinWrap>
+      <PinHead
+        backgroundColor={backgroundColor}
+        textColor={textColor}
+        onClick={onClick}
+      >
+        {content.length > 10 ? content.slice(0, 10) + '...' : content}
+      </PinHead>
+      <PinTail />
+    </PinWrap>
+  );
+};
+
+const PinWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+`;
+
+const PinHead = styled.div<{
+  backgroundColor?: string;
+  textColor?: string;
+}>`
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: ${props => props.backgroundColor || '#37432D'};
+  color: ${props => props.textColor || 'ivory'};
+`;
+
+const PinTail = styled.div<{
+  backgroundColor?: string;
+}>`
+  width: 1rem;
+  border-left: 0.5rem solid transparent;
+  border-right: 0.5rem solid transparent;
+  border-top: 1rem solid ${props => props.backgroundColor || '#37432D'};
+`;
+
+export default Pin;
