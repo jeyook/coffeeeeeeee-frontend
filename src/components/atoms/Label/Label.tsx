50f0d52e9e489bf8c2871f6d929c0b32c6bf6c45
JISUlicious <ljshong@hanmail.net>
2024-02-23 18:34:58 +0900

    style: 기본값 지정 위치 수정

--- src/components/atoms/Label/Label.tsx
+++ src/components/atoms/Label/Label.tsx
@@ -7,7 +7,13 @@ type LabelProps = {
   onClick?: () => void;
 };
 
-const Label = ({ size, color, content, onClick }: LabelProps) => {
+const Label = ({
+  content,
+  size = '1.5rem',
+  color = '#37432D',
+  onClick,
+}: LabelProps) => {
+  // TODO: color 기본값을 디자인 기본 컬러로 변경
   // onClick은 유저 이름, 카페 이름 등에서 기능이 필요하면 사용
   return (
     <LabelWrap size={size} color={color} onClick={onClick}>
@@ -24,7 +30,7 @@ const LabelWrap = styled.div<{
 }>`
   // padding/margin은 Label을 사용하는 wrapper에서 설정하는 편이 좋을 것 같음
   display: flex;
-  font-size: ${props => props.size || '1.5rem'};
-  color: ${props => props.color || '#37432D'}; // TODO: 기본 컬러로 변경
+  font-size: ${props => props.size};
+  color: ${props => props.color};
   font-weight: 700;
 `;
